545c455e56a79a7b83101cf1fba6259acb031c8a
Olga Serbul <Olsen.zika@mail.ru>
2022-11-09 14:10:13 +0300

    практика_на_семинаре_Task01

---
+++ Examples/Практика_на_семинаре/Task01/Program.cs
@@ -0,0 +1,20 @@
+﻿// Напишите программу, которая принимает на вход число (А)
+// и выдаёт сумму чисел от 1 до А.
+
+int FindSum(int number)
+{
+    int sum = 0;
+
+    for (int i = 1; i <= number; i++)
+    {
+        sum = sum + i;
+    }
+    return sum;
+}
+
+Console.Write("Введите число А:  ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+int sum = FindSum(number);
+Console.WriteLine(sum);
+
