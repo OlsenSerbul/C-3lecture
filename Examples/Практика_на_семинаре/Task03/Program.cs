450cee95cb8852171ec645cf771c75f716866619
Olga Serbul <Olsen.zika@mail.ru>
2022-11-09 16:25:41 +0300

    практика_на_семинаре_Task03

---
+++ Examples/Практика_на_семинаре/Task03/Program.cs
@@ -0,0 +1,22 @@
+﻿// Напишите программу, которая принимает на вход число N
+//  и выдаёт произведение чисел от 1 до N.
+
+int FindSqr (int number)
+{
+    string newNumber = number.ToString();
+    int sqr = 1;
+    for (int i = 1; i <= number; i++)
+    {
+        sqr = sqr * i;
+    }
+    return sqr;
+}
+
+Console.Write("Введите число А:  ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+ int sqr = FindSqr (number);
+ Console.WriteLine(sqr);
+
+
+
