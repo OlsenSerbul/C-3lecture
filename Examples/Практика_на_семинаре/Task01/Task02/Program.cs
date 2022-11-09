39ecc23c7114fc40ec05d3cf1d22829058bf3dba
Olga Serbul <Olsen.zika@mail.ru>
2022-11-09 14:36:51 +0300

    практика_на_семинаре_Task02

---
+++ Examples/Практика_на_семинаре/Task01/Task02/Program.cs
@@ -0,0 +1,10 @@
+﻿/* Напишите программу, которая принимает на вход число 
+и выдаёт количество цифр в числе.*/
+// Просим ввести число
+Console.Write("Введите число А:  ");
+int number = Convert.ToInt32(Console.ReadLine());
+// Переводит число в строку, но берем по модулю
+string strNumber = Math.Abs(number).ToString();
+Console.WriteLine(strNumber.Length);
+
+
